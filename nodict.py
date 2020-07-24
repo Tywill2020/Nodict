e1d6b13a32c97b3878dc6c84a6a0f3dc59cd7800
Tyrell Williams <tyrellwilliams2020@gmail.com>
2020-07-24 10:39:13 -0400

    almost finished, one failing test

--- nodict.py
+++ nodict.py
@@ -3,47 +3,53 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = '???'
+__author__ = 'Tyrell Williams'
 
 
 class Node:
     def __init__(self, key, value=None):
-        self.hash = None
-        self.key = None
-        self.value = None
-        # Your code here
-        return
+        self.key = key
+        self.hash = hash(self.key)
+        self.value = value
 
     def __repr__(self):
-        # Your code here
-        return
+        return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
-        # Your code here
-        return
+        return self.key == other.key
 
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        self.buckets = [[] for _ in range(num_buckets)]
+        self.size = num_buckets
+
 
     def __repr__(self):
-        # Your code here
-        return
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i, bucket in enumerate(self.buckets)])
+        
 
-    def add(self, key, value):
-        # Your code here
-        return
+    def add(self, key, value=None):
+        new_node = Node(key, value)
+        bucket = self.buckets[new_node.hash % self.size]
+        for kv in bucket:
+            if kv == new_node:
+                bucket.remove(kv)
+                break
+        bucket.append(new_node)
 
     def get(self, key):
-        # Your code here
-        return
+        key_value = Node(key)
+        bucket = self.buckets[key_value.hash % self.size]
+        for kv in bucket:
+            if kv == key_value:
+                return kv.value
+        raise KeyError(f'{key} Not Found')
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        value = self.get(key)
+        return value
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        self.add(key, value)
+    
